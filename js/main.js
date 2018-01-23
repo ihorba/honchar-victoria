33f4769807a511590e46b3e1699971dd17c6b1f8
ihor@footballaddicts.com <ihor@footballaddicts.com>
2018-01-23 14:15:51 +0200

    menu-2

--- js/main.js
+++ js/main.js
@@ -30,12 +30,60 @@
 		});
 	});
 
-	$('.ba-header__link').on('click', function(e){
+	// Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+	      &&
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 500, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
 
-		$('.ba-header__link').removeClass('active');
-		$(this).addClass('active');
+	  $(document).on('scroll', function () {
+	  	var posDoc = $(this).scrollTop();
 
-	});
+	  	$('.ba-section').each(function (index, item) {
+	  		var topHeader = $(this).offset().top - 38;
+	  		var botHeader = topHeader + $(this).height();
+
+	  		if (
+	  			posDoc > topHeader &&
+	  			posDoc < botHeader
+	  			) {
+	  			$('.ba-header__link').removeClass('active');
+	  		$('.ba-header__link').eq(index).addClass('active');
+	  	}
+
+	  });
+	  });
 
 
 })(jQuery);
